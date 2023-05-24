75d34565bf89b96115049cb52e18961fe9d106ae
Нурсултан <109911696+ZeynKa@users.noreply.github.com>
2023-05-24 16:54:49 +0600

    hw2

---
+++ HW2/main.js
@@ -0,0 +1,46 @@
+// ЗАДАНИЕ 1
+
+// const numbers = [10, 20, 30, 50, 235, 3000, 5000439857];
+//
+// for (let i = 0; i < numbers.length; i++) {
+//     const firstDigit = String(numbers[i])[0];
+//     if (firstDigit === '1' || firstDigit === '2' || firstDigit === '5') {
+//         console.log(numbers[i]);
+//     }
+// }
+
+//
+// ЗАДАНИЕ 2
+//
+// for (let i = 20; i >= 0; i--) {
+//     console.log(i);
+// }
+//
+//  ЗАДАНИЕ 3
+//
+// const color = prompt('Введите цвет светофора:');
+//
+// switch (color) {
+//     case 'красный':
+//         alert('Стоп');
+//         break;
+//     case 'желтый':
+//         alert('Приготовьтесь');
+//         break;
+//     case 'зеленый':
+//         alert('Идите');
+//         break;
+//     default:
+//         alert('Введите корректный цвет светофора');
+//         break;
+// }
+//
+//  ЗАДАНИЕ 4
+//
+// const numbers = [];
+//
+// for (let i = 0; i <= 10; i++) {
+//     numbers.push(i);
+// }
+//
+// console.log(numbers);
